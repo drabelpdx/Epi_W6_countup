a8ae7492c0b096e04d936c5c05577ead41cbb5ae
david abel & jocelyn alsdorf <student@epicodus.com>
2015-05-28 09:40:14 -0700

    Adds initial files and rounds quotient.

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var countUp = function(bigInput, smallInput) {
+  var bigNum = bigInput;
+  var smallNum = smallInput;
+  var countSteps = Math.floor(bigNum/smallNum);
+  return countSteps;
+
+}
---
+++ spec/specs.js
@@ -0,0 +1,6 @@
+describe('countUp', function() {
+  it("rounds down the quotient of the dividend and divisor", function() {
+    expect(countUp(50,7)).to.equal(7);
+  });
+
+});
