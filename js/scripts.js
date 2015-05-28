4e2409a53271e77509bc3c3b313f25c95178d0e6
david abel & jocelyn alsdorf <student@epicodus.com>
2015-05-28 10:18:44 -0700

    Completes functionality. Returns count up to users input.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,27 @@ var countUp = function(bigInput, smallInput) {
   var bigNum = bigInput;
   var smallNum = smallInput;
   var countSteps = Math.floor(bigNum/smallNum);
-  return countSteps;
-
+  var total = 0;
+  var emptyArr = [];
+  for (var i = 0; i < countSteps; i ++) {
+    total += smallNum;
+    emptyArr.push(total);
+  }
+  return emptyArr.join(" ");
 }
+
+
+$(document).ready(function(){
+
+  $("form#count").submit(function(event){
+  var big = parseInt($("input#bigNum").val());
+  var small = parseInt($("input#smallNum").val());
+  var counter = countUp(big, small);
+
+  $("#results").show();
+  $(".result").text(counter);
+
+  event.preventDefault();
+
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -3,4 +3,7 @@ describe('countUp', function() {
     expect(countUp(50,7)).to.equal(7);
   });
 
+  it("it counts up to bigNum by multiples of smallNum", function() {
+    expect(countUp(20,4)).to.eql([4,8,12,16,20]);
+  });
 });
